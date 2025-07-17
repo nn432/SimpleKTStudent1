77c889568eb005d8cbed0e09d4807a98cffb1bcd
qroxfa <tutnetupomoshi@gmail.com>
2025-07-17 04:52:05 +0300

    Update ApplicationDbContext.cs

--- SimpleKTStudent.Infrastructure/ApplicationDbContext.cs
+++ SimpleKTStudent.Infrastructure/ApplicationDbContext.cs
@@ -6,6 +6,8 @@ namespace SimpleKTStudent.Infrastructure
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Testing> Testings { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Config> Configs { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -15,6 +17,37 @@ namespace SimpleKTStudent.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // Configure Testing entity
+            modelBuilder.Entity<Testing>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(255);
+                entity.Property(e => e.Begin).IsRequired();
+                entity.Property(e => e.End).IsRequired();
+                
+                // Configure relationships
+                entity.HasOne(e => e.Student)
+                      .WithMany()
+                      .HasForeignKey(e => e.StudentId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            // Configure Student entity
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(255);
+                entity.Property(e => e.Email).IsRequired().HasMaxLength(255);
+            });
+
+            // Configure Config entity
+            modelBuilder.Entity<Config>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(255);
+                entity.Property(e => e.Value).IsRequired();
+            });
         }
     }
 }
